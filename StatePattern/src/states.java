4301800e194756cc58e75c3a0346c39ff165b20b
Ruchi155 <rap1048@wildcats.unh.edu>
2019-11-10 14:35:00 -0500

    State machine pattern

---
+++ StatePattern/src/states.java
@@ -0,0 +1,15 @@
+
+
+public interface states 
+{
+	public void serviceDisabled();
+	
+	public void serviceEnabled();
+	
+	public void goodhealthCheck();
+	
+	public void badhealthCheck();
+	
+	public void healthCheckTimedOut();
+
+}
